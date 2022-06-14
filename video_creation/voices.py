39ad1a2567685ca1756478c62854a266b577ac6a
joshuapalicka <55159564+joshuapalicka@users.noreply.github.com>
2022-06-14 10:39:57 -0700

    Created envtemplate and updated voices.py
    .envtemplate will now push to GitHub & voices.py now has functionality for selecting different accents using the .env file.

--- video_creation/voices.py
+++ video_creation/voices.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from mutagen.mp3 import MP3
 from utils.console import print_step, print_substep
 from rich.progress import track
+import os
 
 
 def save_text_to_mp3(reddit_obj):
@@ -12,7 +13,15 @@ def save_text_to_mp3(reddit_obj):
     # Create a folder for the mp3 files.
     Path("assets/mp3").mkdir(parents=True, exist_ok=True)
 
-    tts = gTTS(text=reddit_obj["thread_title"], lang="en", slow=False, tld="com.au")
+    if os.getenv("Change Accent") == "TRUE":
+        try:
+            tts = gTTS(text=reddit_obj["thread_title"], lang="en", slow=False, tld=os.getenv("LANGUAGE_ACCENT_CODE"))
+        except ValueError:
+            tts = gTTS(text=reddit_obj["thread_title"], lang="en", slow=False, tld="com.au")
+            print_substep("The language accent code is not valid. Using Australia.")
+    else:
+        tts = gTTS(text=reddit_obj["thread_title"], lang="en", slow=False, tld="com.au"
+                   )
     tts.save(f"assets/mp3/title.mp3")
     length += MP3(f"assets/mp3/title.mp3").info.length
 
